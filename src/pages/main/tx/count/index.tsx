45576503df46f441f315489252cb59cd65e54e5b
fencer-yd <982039620@qq.com>
2019-06-19 09:56:06 +0800

    add tx_router

---
+++ src/pages/main/tx/count/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default class Count extends React.Component<{}, {}> {
+  public render () {
+    return (
+      <div className="tx-count-main"></div>
+    )
+  }
+}
---
+++ src/pages/main/tx/hero/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default class Hero extends React.Component<{}, {}> {
+  public render () {
+    return (
+      <div className="tx-hero-main"></div>
+    )
+  }
+}
